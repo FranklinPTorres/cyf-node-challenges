9f9096c30b6923028b109b9fde04da8e0c861d6d
FranklinPTorres <105072367+FranklinPTorres@users.noreply.github.com>
2023-01-07 14:48:55 +0000

    london website to deploy

---
+++ challenge-london-mini-guide/index.js
@@ -0,0 +1,31 @@
+const express = require ('express');
+const cors = require ('cors');
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req, res) =>{
+    res.send(`This port is working`)
+});
+
+const routes = {
+'/pharmacies': 'returns pharmacies list for stratford',
+'/colleges': 'returns colleges list for stratford',  
+'/doctors': 'returns doctors list for stratford',   
+'/hospitals': 'returns hospitals list for stratford',  
+};
+
+
+
+
+
+
+
+
+const PORT = process.env.PORT || 3001
+
+app.listen(PORT, () => {
+    console.log(`Listening Port ${PORT}`);
+});
+
